f8f75125d1761486565cefca4779ea09bf4e0d82
Administrator <2534243725@qq.com>
2025-09-02 22:06:44 +0800

    格式调整

--- apps/agents/src/react-agent/graph.ts
+++ apps/agents/src/react-agent/graph.ts
@@ -15,8 +15,6 @@ async function callModel(
   const configuration = ensureConfiguration(config);
   const tools = await getTools();
   console.log("Available tools:", tools);
-  
-
   const model = (await loadChatModel(configuration.model)).bindTools(tools);
   const response = await model.invoke([
     {
